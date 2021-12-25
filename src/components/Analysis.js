9c2de12c90b12cfe67c245e77f72e805190886f6
sochi2122 <90932673+sochi2122@users.noreply.github.com>
2021-12-25 13:35:16 -0500

    fixed styling

--- src/components/Analysis.js
+++ src/components/Analysis.js
@@ -5,10 +5,11 @@ function Analysis() {
 
 <h3>Sentiment Analysis</h3>
 
+<p>
 <h1>960</h1>
 <h1>122</h1>
 <h1>321</h1>
-
+</p>
 
         </div>
     );
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,13 +4,14 @@ function Sidebar() {
     return (
         <div className="Sidebar">
 
+<p>
 <h3>Dashboard</h3>
 <h3>Widget</h3>
 <h3>Reviews</h3>
 <h3>Customers</h3>
 <h3>Online Analysis</h3>
 <h3>Settings</h3>
-
+</p>
         </div>
     );
  }
